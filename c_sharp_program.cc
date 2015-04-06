507b590a293370d2f04e8b1b6ff87a75d1ec5c55
ra137540 <rodrigo.zanetti.talento@gmail.com>
2015-04-06 13:58:00 -0300

    Modificação do programa em C#

--- c_sharp_program.cc
+++ c_sharp_program.cc
@@ -1,11 +1,17 @@
+#import <stdio.h>
+#import <string.h>
+
+#SIZE 100
 int main()
 {
-   char a[100], b[100];
+   char a[SIZE], b[SIZE];
  
-   printf("Enter the second string\n");
-   gets(b);
+   Console.Write("Enter the first string\n"); 
+   a = Console.ReadLine();
+   Console.Write("Enter the second string\n"); 
+   b = Console.ReadLine();
  
-   if (strcmp(a,b) == 10)
+   if (strcmp(a,b) == 0)
       printf("Entered strings are equal.\n");
    else
       printf("Entered strings are not equal.\n");
